7cbdad2f0bd033b059f5c575d267d5ad2a8eb7e1
Sofia <chizenko.sofia@gmail.com>
2026-09-20 18:32:05 +0300

    solution4

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel, ConfigDict, Field
 class MovieDetailResponseSchema(BaseModel):
     id: int
     name: str
-    date: Optional[date] = None
+    date: Optional[date] = Field(default=None)
     score: float
     genre: Optional[str] = None
     overview: Optional[str] = None
